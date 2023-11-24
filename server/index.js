028b7d1a3b8efa5cdb07240d970c4f1515c53803
BastD29 <bastiendaniel9@gmail.com>
2023-11-24 19:05:05 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.get("/api/goals", (req, res) => {
+//   res.status(200).json({ message: "Get goals" });
+// });
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
